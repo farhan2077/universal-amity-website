9dfff5d0d70d61a44f775822da5df3ae21e29852
Farhan Bin Amin <farhanbinamin@gmail.com>
2021-01-09 01:40:56 +0600

    final site done_v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 module.exports = {
-    purge: [],
+    purge: {
+        enabled: true,
+        content: ["./*.html"],
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                inter: ["Inter"],
+            },
+            colors: {
+                "custom-yellow": "#DE7C00",
+                "custom-green": "#4E5B31",
+            },
+            margin: {
+                78: "19.5rem",
+            },
+        },
     },
     variants: {
         extend: {},
